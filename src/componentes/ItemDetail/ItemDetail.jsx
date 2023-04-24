4b6d356686820941860d0669ceff87798d4554a7
Roberto Bruno <roberto.brunofl@gmail.com>
2023-04-24 19:18:05 -0300

    Solucionando el route de itemDetail

--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const ItemDetail = ({product}) => {
   return (
     <div>
-      <img src={product.img} alt="" srcset="" /> 
+      <img src={product.img} alt="" /> 
       <h2>{product.title}</h2>
       <h2>{product.description}</h2>
     </div>
